0230b44bf9463ac73e1b71865281aacc68670d80
Khalid Miya <128972346+khaliddracy@users.noreply.github.com>
2023-07-11 07:14:23 +0000

    conversion_operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = null
+// console.log(typeof(score));
+// console.log(typeof score)
+
+let scoreNumber = Number(score)
+// console.log(typeof(scoreNumber));
+// console.log(scoreNumber);
+
+// in conversion all value type will be number but value will be wither number or Nan
+// "88" => 88
+// "88abc" => Nan
+// true => 1
+// null => 0
+// undefined => Nan
+
+let isLoggedIn = "khalid"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false; 
+// "" => false; "khalid"=> true
+
+let num = 4
+let stringNum = String(num)
+// console.log(stringNum);
+// console.log(typeof(stringNum));
+
+
+
+
+
+// ************* Operations *************
+
+
+le  
