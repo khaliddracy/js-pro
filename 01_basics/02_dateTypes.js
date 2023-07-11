a561f98274709a76c139e25fa395f8f8a2315533
Khalid Miya <128972346+khaliddracy@users.noreply.github.com>
2023-07-11 06:46:09 +0000

    datatypes

---
+++ 01_basics/02_dateTypes.js
@@ -0,0 +1,25 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(4+8) -------> we are using nodejs, not browser
+
+ console.log(3+8); console.log("Khalid ") // -> code readability should be high
+
+
+ let name = "khalid"
+ let age = 19
+ let isLoggedin = true
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined => value is not asigned
+ // symbol => unique
+
+
+
+ // object
+
+ console.log(typeof "khalid")
+ console.log(typeof null);
