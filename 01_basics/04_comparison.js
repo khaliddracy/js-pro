e1b7380e5a6ff457f732359ff2032ca11ddd1495
Khalid Miya <128972346+khaliddracy@users.noreply.github.com>
2023-07-11 07:48:25 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0); 
+console.log(null == 0);
+console.log(null >= 0); // comparisons convert null to a number , treating as 0.
+
+console.log("2" == 2);
+console.log("2" === 2);
