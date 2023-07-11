127e9818b21af5854fde60b0b0ddeaae27705cdf
Khalid Miya <128972346+khaliddracy@users.noreply.github.com>
2023-07-11 06:24:10 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 44837
+let accountEmail = "draco@gmail.com"
+var accountPassword =" 48848"
+account = "Kathmandu"
+accountCity = "nepal"
+
+let accountState;
+
+// accountId = 3 not allowed 
+accountEmail = "khalid@gmail.com"
+accountPassword = "8457"
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+
+/* Prefer not to use var because of issue in block scope and functional scope
+.. */
