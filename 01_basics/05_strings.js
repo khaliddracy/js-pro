89988d9d345b70dea775cbca9013edf2d9b27ee6
Khalid Miya <128972346+khaliddracy@users.noreply.github.com>
2023-07-11 11:12:56 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name = "khalid"
+let age = 18
+
+//console.log(name + age +" op"); 
+
+console.log(`Hello my name is ${name} and my age is ${age}`); // prefer do this 
+
+
+
+const superName = new String('Ironman')
+console.log(superName[0])
+console.log(superName.__proto__);
+console.log(superName[2]);
+console.log(superName.indexOf('m'));
+console.log(superName.slice(2,6))
+const newSuper = superName.slice(-3,6)
+console.log(newSuper)
+
+
+const newString = "   khalid   "
+console.log(newString.trim())
+
+const url = "https"
+
